69f483d88c14d45bb70f97eef64527aef81a10ce
wisdom030 <103177888+wisdom030@users.noreply.github.com>
2022-09-14 13:19:22 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$(window).resize(function(){ 
+  if ( window.innerWidth > 1024) {  
+
+		new Swiper('.swiper-container', {
+
+			slidesPerView : 4, 
+			spaceBetween : 30, 
+			slidesPerGroup : 4, 
+			loopFillGroupWithBlank : true,
+		
+			loop : true,
+		
+			pagination : { 
+				el : '.swiper-pagination',
+				clickable : true, 
+			},
+			navigation : { 
+				nextEl : '.swiper-button-next', 
+				prevEl : '.swiper-button-prev', 
+			},
+		});
+
+  } if ( window.innerWidth < 321) {
+
+		new Swiper('.swiper-container', {
+
+			slidesPerView : 2, 
+			spaceBetween : 30, 
+			slidesPerGroup : 1, 
+			loopFillGroupWithBlank : true,
+		
+			loop : true,
+		
+			pagination : { 
+				el : '.swiper-pagination',
+				clickable : true, 
+			},
+			navigation : { 
+				nextEl : '.swiper-button-next', 
+				prevEl : '.swiper-button-prev', 
+			},
+		});
+    
+  }
+  
+  }).resize();
+
+$(".que").click(function() {
+  $(this).next(".anw").stop().slideToggle(300);
+ $(this).toggleClass('on').siblings().removeClass('on');
+ $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+});
+
+$(".subMenuWrap").hide();
+$(".navi > li").mouseover(function() {
+	$(this).find(".subMenuWrap").stop().slideDown(300);
+}).mouseout(function() {
+	$(this).find(".subMenuWrap").stop().slideUp(300);
+});
+
+$(".mNavi").hide();
+$("#tabMenu").click(function() {
+	$(".mNavi").fadeIn();
+});
+$("#close").click(function() {
+	$(".mNavi").fadeOut();
+});
